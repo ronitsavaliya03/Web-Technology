9a2340b225f2ba417a1c54354b8ad85cf299e775
Ronit Savaliya <143884203+ronitsavaliya03@users.noreply.github.com>
2024-08-14 23:51:46 +0530

    Faculty.js

--- ExpressJS/Faculty.js
+++ ExpressJS/Faculty.js
@@ -2,18 +2,50 @@ const express=require('express');
 const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
 const Faculty=require('./Schema');
-const app=express();
 
-mongoose.connect("mongodb+srv://ronitsavaliya:ronit%4005@cluster0.hvbcc.mongodb.net/",{useNewUrlParser: true})
+
+const connectionString='mongodburl';
+mongoose.connect(connectionString)
 .then(()=>{
+    console.log("Connected");
+
     const app=express();
     app.use(bodyParser.urlencoded({extended: false}));
 
-    //code
+    //GetAll
+    app.get('/faculties',async(req,res)=>{
+        const ans=await Faculty.find();
+        res.send(ans);
+    });
 
+    //Create
+    app.post('/faculties',async(req,res)=>{
+        fac=new Faculty({...req.body});
+        const ans=await fac.save();
+        res.send(ans);
+    });
+
+    //Delete
+    app.delete('/faculties/:id',async(req,res)=>{
+        const ans=await Faculty.deleteOne({FacultyID:req.params.id});
+        res.send(ans);
+    });
+
+    //Update
+    app.patch('/faculties/:id',async(req,res)=>{
+        const fac=await Faculty.findOne({FacultyID:req.params.id});
+        fac.FacultyInitial=req.body.FacultyInitial;
+
+        const ans=await fac.save();
+        res.send(ans);
+    });
+
+    
     app.listen(3001,()=>{
         console.log("Success");
     });
 });
+
+
 
 
