da7a1d8819a02b896f7efbbb667a549b2aabeb48
Ronit Savaliya <143884203+ronitsavaliya03@users.noreply.github.com>
2024-08-22 09:21:24 +0530

    Faculty.js

--- ExpressJS/Faculty.js
+++ ExpressJS/Faculty.js
@@ -3,14 +3,37 @@ const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
 const Faculty=require('./Schema');
 
+require('dotenv').config();
 
-const connectionString='mongodburl';
+const connectionString='mongodburl/databasename'
 mongoose.connect(connectionString)
 .then(()=>{
     console.log("Connected");
 
     const app=express();
     app.use(bodyParser.urlencoded({extended: false}));
+
+    //App level Middleware
+    app.use('/',async(req,res,next)=>{
+        console.log("call")   
+        const ans=await Faculty.findOne({
+            FacultyName:req.body.FacultyName,
+            Password:req.body.Password
+        });
+
+        if(ans!==null){
+            next();
+        }
+        else{
+            res.send("unathorized");
+        }
+    });
+
+    //GetOne
+    app.get('/faculties/:name',async(req,res)=>{
+        const ans=await Faculty.findOne({FacultyName:req.params.name});
+        res.send(ans);
+    });
 
     //GetAll
     app.get('/faculties',async(req,res)=>{
@@ -34,18 +57,29 @@ mongoose.connect(connectionString)
     //Update
     app.patch('/faculties/:id',async(req,res)=>{
         const fac=await Faculty.findOne({FacultyID:req.params.id});
-        fac.FacultyInitial=req.body.FacultyInitial;
+        // fac.FacultyInitial=req.body.FacultyInitial;
+        // fac.FacultyID=req.body.FacultyID;
+        // fac.FacultyName=req.body.FacultyName;
+        fac.Password=req.body.Password;
 
         const ans=await fac.save();
         res.send(ans);
     });
 
+    //Search
+    app.get('/faculties/search/:text',async(req,res)=>{
+        const ans=await Faculty.find({
+            FacultyName:{
+                $regex:req.params.text
+            }
+        });
+        res.send(ans);
+    });
+
     
-    app.listen(3001,()=>{
+    app.listen(process.env.port,()=>{
         console.log("Success");
     });
 });
-
-
 
 
